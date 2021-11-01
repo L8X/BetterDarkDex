851d8f9665f5df8cfea253efb565ce2fd19567e6
Madi <49875377+L8X@users.noreply.github.com>
2021-11-01 09:43:03 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,22 +1,18 @@
 local ls = getrenv().loadstring or getfenv(0)['loadstring'] 
+
 _G.loadstr = ls 
 
 local shared_env = getrenv().shared or shared 
 
-shared_env.loadstr = _G.loadstr 
-print(shared_env.loadstr)
+shared_env.loadstr = _G.loadstr print(shared_env.loadstr) 
 
 local new_ls = shared_env.loadstr 
 print(new_ls) 
 
-local function newls(...) 
-  return new_ls(...) 
-end 
+local function newls(...) return new_ls(...) end 
 
 pcall(function() print(newls()) end)
 
-getgenv().new_ls = shared_env.loadstr
-print(new_ls)
+getgenv().new_ls = shared_env.loadstr 
 
-
-
+print(getgenv().new_ls) 
