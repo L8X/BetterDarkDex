75f10bca029a43281dcbefc977db9b0fbeb970b5
Madi <49875377+L8X@users.noreply.github.com>
2021-11-15 11:36:16 +0000

    Update BetterDarkDex.lua

--- BetterDarkDex.lua
+++ BetterDarkDex.lua
@@ -1,20 +1,3 @@
--- syndexpro.lua --
-
-local function NSIK_fake_script()
-local script = Instance.new('LocalScript')	
-setthreadidentity(2)
-			
-getgenv().Services = setmetatable({},{__index=function(s,r) return game:service(r) end})	
-			
-getgenv().Protector = loadstring(game:HttpGet("https://raw.githubusercontent.com/pamlib/prote.lua/ca01e9b8b3478762370d4a1d1ee65bae6ee881a3/main.lua", true, Enum.HttpRequestType.Analytics, true))
-			
-getgenv().confi = loadstring(game:HttpGet("https://l8x.github.io/syndexpro/confi.lua", true, Enum.HttpRequestType.Analytics, true))
-
-loadstring(game:HttpGet("https://pastebin.com/raw/ri3pGiYz", true, Enum.HttpRequestType.Analytics, true))()	
-			
-loadstring(game:HttpGet("https://pastebin.com/raw/AGBj7SXt", true, Enum.HttpRequestType.Analytics, true))
-
-pcall(function()
 local OldIndex
 OldIndex = hookmetamethod(game, "__index", function(Self, Index)
     return OldIndex(Self, Index)
@@ -62,86 +45,41 @@ local function get_proxy_game_metatable(x)
  return setmetatable(proxy,proxy_mt)
 end
 hookfunction(getrawmetatable,get_proxy_game_metatable)
-end)
 
-local memCheckBypass
+getrenv().error = function() end
+getrenv().warn = function() end
+getrenv().print = function() end
 
-memCheckBypass = hookfunction(getrenv().gcinfo, function(...)
-   --warn("Script tried to memory check, PATH: \n"..debug.traceback())
-   return tonumber(math.random(55-math.random(1,45), 110-math.random(1,35)*0.215-math.random(1, 45)))
-end)
+getgenv().Services = setmetatable({},{__index=function(s,r) return game:service(r) end})
 
-local CCoreGui = cloneref(Services.CoreGui)
-local CContentProvider = cloneref(Services.ContentProvider)
-local CInsertService = cloneref(Services.InsertService)
+getgenv().Protector = loadstring(game:HttpGet("https://raw.githubusercontent.com/pamlib/prote.lua/ca01e9b8b3478762370d4a1d1ee65bae6ee881a3/main.lua", true, Enum.HttpRequestType.Analytics, true))
 
--- < Functions > --
-function gethui()
-return CCoreGui
-end
+-- < Services > --
+local InsertService = Services.InsertService
+local CoreGui = Services.CoreGui
+local ScriptContext = Services.ScriptContext
+local ContentProvider = Services.ContentProvider
 
-getgenv().yeetdex = function(yeetdex)
-local CoreGui = gethui()
-local RemoteDebugWindow = CoreGui:FindFirstChild("RemoteDebugWindow", true)
-if RemoteDebugWindow then
-    RemoteDebugWindow.Parent:Destroy()
-end end
+Services.HttpService:SetHttpEnabled(true)
+settings()['Task Scheduler'].ThreadPoolConfig = Enum.ThreadPoolConfig.Threads16
 
--- < Services > --	
-local InsertService = CInsertService
-local CoreGui = gethui()
-local ContentProvider = CContentProvider
+ScriptContext:SetTimeout(5)
 -- < Aliases > --
-local table_insert = table.insert
-local table_foreach = table.foreach
-local string_char = string.char
 getgenv().getobjects = function(a)
     local Objects = {}
     if a then
         local b = InsertService:LoadLocalAsset(a)
         if b then 
-            table_insert(Objects, b) 
+            table.insert(Objects, b) 
         end
     end
     return Objects
 end
 
--- < Values > --
-local Charset = {}
-local Random_Instance = Random.new()
-local RemoteDebugWindow = CoreGui:FindFirstChild("RemoteDebugWindow", true)
--- < Source > --
-if RemoteDebugWindow then
-	RemoteDebugWindow.Parent:Destroy()
-end
+local Dex = getobjects("rbxassetid://6827450620")[1]
 
-for i = 48,  57 do 
-	table_insert(Charset, string_char(i))
-end
+ContentProvider:Preload("rbxassetid://6827450620")
 
-for i = 65,  90 do 
-	table_insert(Charset, string_char(i))
-end
-
-for i = 97, 122 do
-	table_insert(Charset, string_char(i))
-end
-
-function RandomCharacters(length)
-	return length > 0 and RandomCharacters(length - 1)..Charset[Random_Instance:NextInteger(1, #Charset)] or ""
-end
-			
-local HTTPService = cloneref(Services.HttpService)
-local CoreGui     = gethui()
-local ScriptContext = cloneref(Services.ScriptContext)
-local RandomObject = CoreGui:FindFirstChildOfClass("ScreenGui")
-local RandomObject2 = Instance.new("Folder", RandomObject)
-syn.protect_gui(RandomObject2)
-local CRandomObject2 = cloneref(RandomObject2)
-syn.protect_gui(CRandomObject2)
-
-local Dex = cloneref(getobjects("rbxassetid://7995973532")[1])
-ContentProvider:Preload("rbxassetid://7995973532")
 task.spawn(function()
 task.synchronize()
 for i,v in pairs(Dex:GetDescendants()) do
@@ -149,50 +87,103 @@ for i,v in pairs(Dex:GetDescendants()) do
     end
 task.wait(0)
 end)
-Dex.Name = "RobloxGui" -- bypass attempt??
-sethiddenproperty(Dex, "OnTopOfCoreBlur", true)
-sethiddenproperty(Dex, "AutoLocalize", true)
-sethiddenproperty(Dex, "Localize", true)
-sethiddenproperty(Dex, "IgnoreGuiInset", true)
-sethiddenproperty(Dex, "DisplayOrder", 2147483647)
-sethiddenproperty(cloneref(Services.UserInputService), "GazeSelectionEnabled", true)
-sethiddenproperty(cloneref(Services.StarterGui), "ProcessUserInput", true)
+
+Dex.Name = "RobloxGui"
 syn.protect_gui(Dex)
-syn.protect_gui(RandomObject)
-syn.protect_gui(RandomObject2)
-syn.protect_gui(CRandomObject2)
+Dex.Parent = Services.CoreGui
 Protector():ProtectInstance(Dex, true)
-Protector():ProtectInstance(RandomObject2, true)
-Protector():ProtectInstance(CRandomObject2, true)
-Dex.Parent = CRandomObject2
-	
+Protector():ProtectInstance(Dex.Parent, true)
+Protector():SpoofInstance(Dex, Dex)
+Protector():SpoofInstance(Dex.Parent, Dex.Parent)
+syn.protect_gui(Dex.Parent)
+
 local function Load(Obj, Url)
-	local function GiveOwnGlobals(Func, Script)
-		local Fenv, RealFenv, FenvMt = {}, {script = Script}, {}
-		FenvMt.__index = function(a,b)
-			return RealFenv[b] == nil and getgenv()[b] or RealFenv[b]
-		end
-		FenvMt.__newindex = function(a, b, c)
-			if RealFenv[b] == nil then 
-				getgenv()[b] = c
-		else 
-			RealFenv[b] = c 
-			end
-		end
-		setmetatable(Fenv, FenvMt)
-		pcall(setfenv, Func, Fenv)
-		return Func
-	end
-	local function LoadScripts(_, Script)
-		if Script:IsA("LocalScript") then
-			spawn(function()
-				GiveOwnGlobals(loadstring(Script.Source,"="..Script:GetFullName()), Script)()
-			end)
-		end
-		table_foreach(Script:GetChildren(), LoadScripts)
-	end
-LoadScripts(nil, Obj)
+local function GiveOwnGlobals(Func, Script)
+    local Fenv = {}
+    local RealFenv = {script = Script}
+    local FenvMt = {}
+    FenvMt.__index = function(a,b)
+        if RealFenv[b] == nil then
+            return getgenv()[b]
+        else
+            return RealFenv[b]
+        end
+    end
+    FenvMt.__newindex = function(a, b, c)
+        if RealFenv[b] == nil then
+            getgenv()[b] = c
+        else
+            RealFenv[b] = c
+        end
+    end
+    setmetatable(Fenv, FenvMt)
+    setfenv(Func, Fenv)
+    return Func
+end
+local function LoadScripts(Script)
+    if Script.ClassName == "Script" or Script.ClassName == "LocalScript" then
+        spawn(function()
+            GiveOwnGlobals(loadstring(Script.Source, "=" .. Script:GetFullName()), Script)()
+        end)
+    end
+    for i,v in pairs(Script:GetChildren()) do
+            syn.protect_gui(v)
+    LoadScripts(v)
+    end
+end
+LoadScripts(Obj)
+syn.protect_gui(Obj)
 end
 Load(Dex)
+syn.protect_gui(Dex)
+Protector():ProtectInstance(Dex, true)
+Protector():SpoofInstance(Dex, Dex)
+
+for i, v in next, getconnections(Services.UserInputService.TextBoxFocused) do
+    v:Disable()
 end
-coroutine.wrap(NSIK_fake_script)()
+for i, v in next, getconnections(Services.UserInputService.TextBoxFocusReleased) do
+    v:Disable()
+end 
+
+spawn(function()
+while Services.RunService.RenderStepped:Wait() do
+for i, v in next, getconnections(Services.UserInputService.TextBoxFocused) do
+    v:Disable()
+end
+for i, v in next, getconnections(Services.UserInputService.TextBoxFocusReleased) do
+    v:Disable()
+end
+end
+end)
+
+task.spawn(function()
+task.synchronize()
+Protector():ProtectInstance(Dex, true)
+Protector():ProtectInstance(Dex, Dex)
+task.wait()
+end)
+
+Protector():ProtectInstance(Dex)
+Protector():ProtectInstance(Dex, true)
+
+
+Inputting = false
+ChatBar = nil
+Current = nil
+
+function Check()
+	wait(.1)
+	Inputting = false
+	Disconnection:Disconnect()
+end
+
+function InputBegan()
+	if game:GetService("UserInputService"):GetFocusedTextBox() then
+		ChatBar = game:GetService("UserInputService"):GetFocusedTextBox()
+		Inputting = true
+		Current = ChatBar.FocusLost
+		Disconnection = Current:Connect(Check)
+	end
+end
+InputConnect = game:GetService("UserInputService").InputBegan:Connect(InputBegan)
